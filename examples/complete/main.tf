3d318d3f9e80790d074f5ee376ded7078689f0fb
Daniel Schmidt <79086712+danischm@users.noreply.github.com>
2021-09-26 08:37:39 +0200

    Initial commit

---
+++ examples/complete/main.tf
@@ -0,0 +1,8 @@
+module "aci_scaffolding" {
+  source  = "netascode/scaffolding/aci"
+  version = ">= 0.0.1"
+
+  name        = "ABC"
+  alias       = "ABC-ALIAS"
+  description = "My Description"
+}
